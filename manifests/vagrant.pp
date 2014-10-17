d24925e7c68355567f07a9ba7d5c27b4e11a975a
Ryan Rauh <rauh.ryan@gmail.com>
2014-10-17 10:31:51 -0500

    Merge commit

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -56,4 +56,37 @@ node /^.*jenkinsmaster$/ inherits default {
   Package {
     require => Class['apt']
   }
+  $databases = ["huboard_enterprise_dev", "huboard_enterprise_test"]
+  $users = ["jenkins", "ubuntu"]
+
+  class { 'postgresql::globals':
+    version             => '9.3',
+    manage_package_repo => true,
+    encoding            => 'UTF8',
+    } ->
+    class { 'postgresql::server': } ->
+    class { 'postgresql::lib::devel': }
+
+    postgresql::server::pg_hba_rule { 'local-users-get-everything':
+      type        => 'local',
+      database    => 'all',
+      user        => 'all',
+      auth_method => 'trust',
+      order       => '0001',
+    }
+    postgresql::server::pg_hba_rule { 'local-host-connections-get-everything':
+      type        => 'host',
+      database    => 'all',
+      user        => 'all',
+      address     => '127.0.0.1/32',
+      auth_method => 'trust',
+      order       => '0001',
+    }
+    postgresql::server::database { $databases:
+    }
+    postgresql::server::role { $users:
+      superuser     => true,
+      createdb      => true,
+      createrole    => true,
+    }
 }
